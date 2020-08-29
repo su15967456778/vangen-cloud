e6feb7e39396fe15245907f34c3d21fdd2b265a1
苏凌峰 <511045477@qq.com>
2020-08-29 14:21:24 +0800

    modify file

--- vangen-zipkin/src/main/java/vangencloud/vangenzipkin/VangenZipkinApplication.java
+++ vangen-zipkin/src/main/java/vangencloud/vangenzipkin/VangenZipkinApplication.java
@@ -2,8 +2,12 @@ package vangencloud.vangenzipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class VangenZipkinApplication {
 
     public static void main(String[] args) {
