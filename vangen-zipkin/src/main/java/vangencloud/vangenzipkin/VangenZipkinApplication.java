36f14464697dedf9edec185e781ce4f6354035bc
苏凌峰 <511045477@qq.com>
2020-08-29 14:02:36 +0800

    add zipkin

---
+++ vangen-zipkin/src/main/java/vangencloud/vangenzipkin/VangenZipkinApplication.java
@@ -0,0 +1,13 @@
+package vangencloud.vangenzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VangenZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VangenZipkinApplication.class, args);
+    }
+
+}
