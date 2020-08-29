9d1102a431ce54510c992d42a3638964c5ec2d8b
苏凌峰 <511045477@qq.com>
2020-08-29 13:20:10 +0800

    modify file

---
+++ vangen-config/src/main/java/vangencloud/vangenconfig/VangenConfigApplication.java
@@ -0,0 +1,17 @@
+package vangencloud.vangenconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaServer
+public class VangenConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VangenConfigApplication.class, args);
+    }
+
+}
